2911ccf23045d67b5eeb03375ed9c68b5051ee9e
Joe <92691827+shaggameister@users.noreply.github.com>
2023-03-23 18:01:01 -0400

    removed unnecessary section tags

--- index.js
+++ index.js
@@ -5,28 +5,26 @@ const main = document.getElementById("main")
 let html=""
 
 for(let i = 0; i<posts.length; i++) {
-    html += `
-    <section>
-        <div class="container">
-            <div class="section-header">
-                <img class="avatar" src="${posts[i].avatar}" alt="${posts[i].name}'s profile picture">
-                <div>
-                    <p class="name">${posts[i].name}</p>
-                    <p class="location">${posts[i].location}</p>
-                </div>
-            </div>
-            <img class="portrait" src="${posts[i].post}" alt="${posts[i].name}'s portrait">
-            <div class="section-footer">
-                <div>
-                    <img class="icon" src="images/icon-heart.png" alt="heart like icon">
-                    <img class="icon" src="images/icon-comment.png" alt="comment icon">
-                    <img class="icon" src="images/icon-dm.png" alt="direct message icon">
-                </div>
-                <p class="likes"><span>${posts[i].likes} likes</p></span><p>
-                <p class="user-id"><span>${posts[i].username}</span> ${posts[i].comment}</p>
+    html += /*html*/`
+    <div class="container">
+        <div class="section-header">
+            <img class="avatar" src="${posts[i].avatar}" alt="${posts[i].name}'s profile picture">
+            <div>
+                <p class="name">${posts[i].name}</p>
+                <p class="location">${posts[i].location}</p>
             </div>
         </div>
-    </section>
+        <img class="portrait" src="${posts[i].post}" alt="${posts[i].name}'s portrait">
+        <div class="section-footer">
+            <div>
+                <img class="icon" src="images/icon-heart.png" alt="heart like icon">
+                <img class="icon" src="images/icon-comment.png" alt="comment icon">
+                <img class="icon" src="images/icon-dm.png" alt="direct message icon">
+            </div>
+            <p class="likes"><span>${posts[i].likes} likes</p></span><p>
+            <p class="user-id"><span>${posts[i].username}</span> ${posts[i].comment}</p>
+        </div>
+    </div>
     `
 }
 
